d1de26888a1e31edbcc669a080a5479d7246d217
Rahul Tanwar <mr.rahultanwar@gmail.com>
2021-10-24 21:32:28 +0530

    code refactoring

--- RAT/datahandler.js
+++ RAT/datahandler.js
@@ -1,285 +1,163 @@
 //Dependencies
-var fh = require('./filehandler');
+var fh = require("./filehandler");
 var objectPath = require("object-path");
 let JSONFormatter = require("simple-json-formatter");
 
 //Container of datahandler.js
 
 var data = {
-
   dashboard: {},
-  api: {}
+  api: {},
 };
 
 //handlers for dashboard
 
-
-
 data.dashboard.index = function (callback) {
-
   fh.list(function (err, data) {
-
     if (!err) {
-
       var returnData = {
-
-        jsonData: 'Select DB',
+        jsonData: "Select DB",
         dataOut: data,
-        file: 'index'
-
-      }
+        file: "index",
+      };
 
       callback(returnData);
-
     }
-
-
   });
-
-
 };
 
 data.dashboard.view = function (file, callback) {
-
   fh.list(function (err, data) {
-
     if (!err) {
-
       fh.read(file, function (err, data2) {
-
-
         if (!err) {
-
           let formattedJson = JSONFormatter.format(JSON.stringify(data2));
 
           var returnData = {
-
             jsonData: formattedJson,
             dataOut: data,
-            file: 'index'
-
-          }
+            file: "index",
+          };
 
           callback(false, returnData);
-
-
-
-
         } else {
-
           callback(true, returnData);
         }
-
-
       });
-
     }
-
-
   });
-
-
-
-
 };
 
 //handlers for api requests
 
-
 //create file
 data.api.create = function (file, callback) {
-
   var dt = {};
   objectPath.set(dt, file, {});
 
-
-
   fh.create(file, dt, function (err) {
-
     if (!err) {
-
-      callback(false, 'DB Created Successfully');
-
+      callback(false, "DB Created Successfully");
     } else {
-
-      callback(true, 'DB Creation UnSuccessfull');
+      callback(true, "DB Creation UnSuccessfull");
     }
-
   });
-
-
 };
 
 //view file
 
 data.api.view = function (file, callback) {
-
   fh.read(file, function (err, data) {
-
-
     if (!err) {
-
-
-
       callback(false, JSON.stringify(data));
-
-
-
-
     } else {
-
-      callback(false, 'Error in file loading');
+      callback(false, "Error in file loading");
     }
-
-
   });
-
 };
 
 //view file
 
 data.api.delete = function (file, callback) {
-
   fh.delete(file, function (err) {
-
     if (!err) {
-
-      callback(false, 'File Deleted Successfully');
-
+      callback(false, "File Deleted Successfully");
     } else {
-
-      res.send(true, 'File Not Deleted');
+      res.send(true, "File Not Deleted");
     }
-
-
   });
-
 };
-
 
 //insert data
 
 data.api.insert = function (file, callback) {
-
   var _location = file.location;
   var _data = file.data;
   console.log(file.location);
   fh.read(file.id, function (err, data) {
-
     _data = JSON.parse(_data);
 
     if (!err) {
-
-
       objectPath.push(data, _location, _data);
       fh.update(file.id, data, function (err) {
-
         if (!err) {
-
           callback(false, "File Updated");
-
         } else {
-
           callback(true, "Error in File Updation");
         }
-
       });
-
     } else {
-
       callback(true, "Error in reading file");
     }
-
-
   });
-
-
-
 };
 
 //delete data
 data.api.deleteData = function (file, callback) {
-
   var _location = file.location;
   console.log(file.location);
   fh.read(file.id, function (err, data) {
-
     if (!err) {
-
-
       //JSON.parse(_location)
 
       objectPath.del(data, _location);
 
-
       fh.update(file.id, data, function (err) {
-
         if (!err) {
-
           callback(false, "File Updated");
-
         } else {
-
           callback(true, "Error in File Updation");
         }
-
       });
-
     } else {
-
       callback(true, "Error in reading file");
     }
-
-
   });
-
-
-
 };
 
 //set data
 data.api.setData = function (file, callback) {
-
   var _location = file.location;
   var _data = file.data;
   console.log(file.location);
   fh.read(file.id, function (err, data) {
-
     if (!err) {
-
-
       //JSON.parse(_location)
 
       objectPath.set(data, _location, JSON.parse(_data));
 
-
       fh.update(file.id, data, function (err) {
-
         if (!err) {
-
           callback(false, "File Updated");
-
         } else {
-
           callback(true, "Error in File Updation");
         }
-
       });
-
     } else {
-
       callback(true, "Error in reading file");
     }
-
-
   });
-
-
-
 };
-
-
-
 
 //Export the Module
 
--- RAT/server.js
+++ RAT/server.js
@@ -1,48 +1,42 @@
 // Dependencies
-const express = require('express');
+const express = require("express");
 const app = express();
-var bodyParser = require('body-parser');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
+var bodyParser = require("body-parser");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
 
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({
-    extended: true
-})); // support encoded bodies
-app.set('view engine', 'ejs');
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+); // support encoded bodies
+app.set("view engine", "ejs");
 app.use(fileUpload());
 
+var path = require("path");
 
-var path = require('path');
+var config = require("../config");
+var dH = require("./datahandler");
+var oH = require("./objecthandler");
 
-
-var config = require('../config');
-var dH = require('./datahandler');
-var oH = require('./objecthandler');
-
-
-// Container for server 
+// Container for server
 var server = {};
 
 server.init = function () {
+  //Server Connection
+  app.listen(config.serverDetails.port, () => {
+    console.log(
+      "\x1b[35m%s\x1b[0m",
+      "Server started on port " + config.serverDetails.port + "."
+    );
+  });
+};
 
-
-    //Server Connection
-    app.listen(config.serverDetails.port, () => {
-        console.log('\x1b[35m%s\x1b[0m', 'Server started on port ' + config.serverDetails.port + '.');
-    });
-
-
-
-}
-
-
-app.get('/', function (req, res) {
-
-    res.send("Hello world");
-
+app.get("/", function (req, res) {
+  res.send("Hello world");
 });
 
 //??????????????????????????????//
@@ -50,279 +44,180 @@ app.get('/', function (req, res) {
 //Funtions for handling the dashboard
 
 //Index View
-app.get('/dashboard', function (req, res) {
-
-    dH.dashboard.index(function (data) {
-
-
-        res.render(data.file, {
-            jsonData: data.jsonData,
-            dataOut: data.dataOut,
-            projectName: config.projectDetails["projectName"]
-        });
-
-
+app.get("/dashboard", function (req, res) {
+  dH.dashboard.index(function (data) {
+    res.render(data.file, {
+      jsonData: data.jsonData,
+      dataOut: data.dataOut,
+      projectName: config.projectDetails["projectName"],
     });
-
+  });
 });
 
 //View the DB
-app.get('/dashboard/view/:id', function (req, res) {
-
-    dH.dashboard.view(req.params.id, function (err, data) {
-
-
-        if (!err) {
-            res.render(data.file, {
-                jsonData: data.jsonData,
-                dataOut: data.dataOut,
-                projectName: config.projectDetails["projectName"]
-
-            });
-
-        } else {
-
-            res.send("Error: File Not Found");
-
-        }
-
-
-
-    });
-
+app.get("/dashboard/view/:id", function (req, res) {
+  dH.dashboard.view(req.params.id, function (err, data) {
+    if (!err) {
+      res.render(data.file, {
+        jsonData: data.jsonData,
+        dataOut: data.dataOut,
+        projectName: config.projectDetails["projectName"],
+      });
+    } else {
+      res.send("Error: File Not Found");
+    }
+  });
 });
 //??????????????????????????//
 
-app.get('/help/', (req, res) => {
-
-    res.sendFile(path.join(__dirname, '../views/pages/', 'help.html'));
-
+app.get("/help/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/pages/", "help.html"));
 });
 
 //??????????????????????????//
 //Funtions for handling the api
 
-//create file 
-app.post('/api/create/', (req, res) => {
+//create file
+app.post("/api/create/", (req, res) => {
+  var _file = req.body.file;
 
-    var _file = req.body.file;
-
-    dH.api.create(_file, function (err, result) {
-
-        if (!err) {
-
-            res.send(result);
-        } else {
-
-            res.send(result);
-        }
-
-
-    });
-
-
-
+  dH.api.create(_file, function (err, result) {
+    if (!err) {
+      res.send(result);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
-//view file  
+//view file
 
-app.post('/api/view/', (req, res) => {
+app.post("/api/view/", (req, res) => {
+  var _file = req.body.file;
 
-    var _file = req.body.file;
-
-    dH.api.view(_file, function (err, result) {
-
-        if (!err) {
-
-            res.send(result);
-        } else {
-
-            res.send(result);
-        }
-
-
-    });
-
-
-
+  dH.api.view(_file, function (err, result) {
+    if (!err) {
+      res.send(result);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
-//Delete File 
-app.post('/api/delete/', (req, res) => {
+//Delete File
+app.post("/api/delete/", (req, res) => {
+  var _file = req.body.file;
 
-    var _file = req.body.file;
-
-    dH.api.delete(_file, function (err, result) {
-
-        if (!err) {
-
-            res.send(result);
-        } else {
-
-            res.send(result);
-        }
-
-    });
-
-
-
+  dH.api.delete(_file, function (err, result) {
+    if (!err) {
+      res.send(result);
+    } else {
+      res.send(result);
+    }
+  });
 });
-
 
 //Insert Data
 
-app.post('/api/insert/', (req, res) => {
+app.post("/api/insert/", (req, res) => {
+  var _location = req.body.location;
+  var _data = req.body.data;
 
+  var file = {
+    id: req.body.file,
+    location: _location,
+    data: _data,
+  };
 
-    var _location = req.body.location;
-    var _data = req.body.data;
-
-    var file = {
-
-        "id": req.body.file,
-        "location": _location,
-        "data": _data
-
+  dH.api.insert(file, function (err, result) {
+    if (!err) {
+      res.send(JSON.stringify(result));
+    } else {
+      res.send(JSON.stringify(result));
     }
-
-    dH.api.insert(file, function (err, result) {
-
-        if (!err) {
-
-            res.send(JSON.stringify(result));
-        } else {
-
-            res.send(JSON.stringify(result));
-        }
-
-    });
-
-
+  });
 });
-
 
 //Delete Data
 
-app.post('/api/deletedata/', (req, res) => {
+app.post("/api/deletedata/", (req, res) => {
+  var _location = req.body.location;
 
+  var file = {
+    id: req.body.file,
+    location: _location,
+  };
 
-    var _location = req.body.location;
-
-    var file = {
-
-        "id": req.body.file,
-        "location": _location,
-
+  dH.api.deleteData(file, function (err, result) {
+    if (!err) {
+      res.send(result);
+    } else {
+      res.send(result);
     }
-
-    dH.api.deleteData(file, function (err, result) {
-
-        if (!err) {
-
-            res.send(result);
-        } else {
-
-            res.send(result);
-        }
-
-    });
-
-
+  });
 });
 
 //set Data
-app.post('/api/setdata/', (req, res) => {
+app.post("/api/setdata/", (req, res) => {
+  var _location = req.body.location;
+  var _data = req.body.data;
 
+  var file = {
+    id: req.body.file,
+    location: _location,
+    data: _data,
+  };
 
-    var _location = req.body.location;
-    var _data = req.body.data;
-
-    var file = {
-
-        "id": req.body.file,
-        "location": _location,
-        "data": _data
-
+  dH.api.setData(file, function (err, result) {
+    if (!err) {
+      res.send(result);
+    } else {
+      res.send(result);
     }
-
-    dH.api.setData(file, function (err, result) {
-
-        if (!err) {
-
-            res.send(result);
-        } else {
-
-            res.send(result);
-        }
-
-    });
-
-
+  });
 });
 
+//findindex
 
-//findindex 
+app.post("/api/findindex/", (req, res) => {
+  var _location = req.body.location;
+  var _compareto = req.body.compareto;
+  var _comparedata = req.body.comparedata;
 
-app.post('/api/findindex/', (req, res) => {
+  var file = {
+    id: req.body.file,
+    location: _location,
+    compareto: _compareto,
+    comparedata: _comparedata,
+  };
 
-
-    var _location = req.body.location;
-    var _compareto = req.body.compareto;
-    var _comparedata = req.body.comparedata;
-    
-
-
-    var file = {
-
-        "id": req.body.file,
-        "location": _location,
-        "compareto" : _compareto,
-        "comparedata" : _comparedata
-
+  oH.findIndex(file, function (err, result) {
+    if (!err) {
+      res.send(result);
+    } else {
+      res.send(result);
     }
-
-    oH.findIndex(file,function(err,result){
-
-        if(!err){
-
-            res.send(result);
-
-        }else{
-
-            res.send(result);
-
-        }
-
-
-    });
-
-    
-
-
+  });
 });
-
 
 //post method for file upload
 
+app.post("/api/upload", function (req, res) {
+  if (Object.keys(req.files).length == 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
 
-app.post('/api/upload', function(req, res) {
-    if (Object.keys(req.files).length == 0) {
-      return res.status(400).send('No files were uploaded.');
-    }
-  
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    let sampleFile = req.files.file;
-  
-    // Use the mv() method to place the file somewhere on your server
-    var uploadPath = path.join(__dirname, '../Uploads/', sampleFile.name);
-    sampleFile.mv(uploadPath, function(err) {
-      if (err)
-        return res.status(500).send(err);
-  
-      res.send('File uploaded!');
-    });
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.file;
+
+  // Use the mv() method to place the file somewhere on your server
+  var uploadPath = path.join(__dirname, "../Uploads/", sampleFile.name);
+  sampleFile.mv(uploadPath, function (err) {
+    if (err) return res.status(500).send(err);
+
+    res.send("File uploaded!");
   });
+});
 
-
-//Export the module 
+//Export the module
 module.exports = server;
